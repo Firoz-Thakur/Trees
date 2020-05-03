04bea8c4979feea0121aaaa200a667450c7e539d
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-05-03 13:58:43 +0530

    Update traversal_of_bst_pre_post_inorder.cpp

--- traversal_of_bst_pre_post_inorder.cpp
+++ traversal_of_bst_pre_post_inorder.cpp
@@ -116,6 +116,92 @@ void levelOrdermodified(node* root)
 	
 }
 
+int countnode(node* root)
+{
+	if(root==NULL)
+	 return 0;
+   
+   int nodeleft=countnode(root->left);
+   int noderight=countnode(root->right);
+   
+   return (1+nodeleft+noderight);
+ 
+}
+
+int sum_of_node_of_tree(node* root)
+{
+	if(root==NULL)
+	 return 0;
+ 	
+ int leftsum=sum_of_node_of_tree(root->left);
+ int rightsum=sum_of_node_of_tree(root->right);
+ 
+ return root->data+leftsum+rightsum;	
+}
+
+int height(node* root)
+{
+	if(root==NULL)
+	 return 0;
+    
+   int h1= height(root->left);
+   int h2=height(root->right);
+    
+   return 1+ max(h1,h2); 
+}
+
+class Pair
+{
+	public :
+	int height;
+	int diamter;
+};
+
+
+ 
+Pair diameter(node* root)
+{
+	Pair p;
+	
+	if(root==NULL)
+	 {
+	 p.height=0;
+	 p.diamter=0;
+	 return p;
+	 }
+	
+	Pair left=diameter(root->left);
+	Pair right=diameter(root->right);
+	
+    p.height=1+max(left.height,right.height);
+    
+    p.diamter=max(left.height+right.height,max(left.diamter,right.diamter));
+    
+    return p;
+	
+}
+ 
+int sumreplacement(node* root)
+{
+	if(root==NULL)
+	 return 0;
+	 
+	if(root->left==NULL && root->right==NULL)
+	 return root->data;
+	 
+    int leftsum=sumreplacement(root->left);
+    int rightsum=sumreplacement(root->right);
+	
+	int temp=root->data;
+	
+	root->data=leftsum+rightsum;
+	
+	return temp+root->data;
+}
+
+
+
+
 int main() {
 	// your code goes here
  
@@ -140,25 +226,79 @@ int main() {
  cout<<"level order with NULL value"<<endl;
  levelOrdermodified(root);
  
+ cout<<endl;
+ 
+ cout<<"count the number of node in the tree"<<endl;
+ cout<<countnode(root);
+ 
+ cout<<endl;
+ cout<<"sum of node of the tree"<<endl;
+ 
+ cout<<sum_of_node_of_tree(root);
+ 
+  cout<<endl;
+ cout<<"height of the tree"<<endl;
+
+ cout<<height(root);
+
+ cout<<endl;
+ cout<<"diameter of the tree"<<endl;
+ 
+ Pair x=diameter(root);
+ cout<<x.diamter;
+ cout<<endl;
+ 
+ cout<<"height of the tree is"<<endl;
+
+ cout<<x.height<<endl;
+ 
+ //replacement of the node by the decendent sums
+ cout<<"level order traversal of the tree"<<endl;
+ levelOrdermodified(root);
+ cout<<endl;
+ 
+ sumreplacement(root);
+ cout<<"level order traversal after replacement"<<endl;
+ 
+ levelOrdermodified(root);
+ 
  
  return 0;
 }
 
 
 
-5 4 2 -1 -1 1 -1 -1 2  3 -1 -1 4 -1 -1
-
 preorder
-5 4 2 1 2 3 4 
+5 4 2 1 2 
 postorder
-2 1 4 3 4 2 5 
+2 1 4 2 5 
 inorder
-2 4 1 5 3 2 4 
+2 4 1 5 2 
 level order
-5 4 2 2 1 3 4 
+5 4 2 2 1 
 level order with NULL value
 5 
 4 2 
-2 1 3 4 
+2 1 
+
+count the number of node in the tree
+5
+sum of node of the tree
+14
+height of the tree
+3
+diameter of the tree
+3
+height of the tree is
+3
+level order traversal of the tree
+5 
+4 2 
+2 1 
+
+level order traversal after replacement
+9 
+3 2 
+2 1 
 
 
