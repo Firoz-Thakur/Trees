385675b3eb8e47c4ee466dd5ba3261cab0f4ba6f
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-05-26 08:35:37 +0530

    Create left view of binary tree using recursion.cpp

---
+++ left view of binary tree using recursion.cpp
@@ -0,0 +1,23 @@
+
+
+
+int max_depth=-1;
+
+void solve(Node* root,int depth) {
+if(!root)
+return;
+if(depth>max_depth)
+{
+cout<<root->data<<" ";
+max_depth=depth;
+}
+
+solve(root->left,depth+1);
+solve(root->right,depth+1);
+
+}
+void leftView(Node *root)
+{
+max_depth=-1; // the declaration of this is compulsory
+solve(root,0);
+}
